0845d33a8a22dfea69b986484859dbdc6abe17ea
Lukem512 <lukem95@gmail.com>
2014-04-13 09:27:10 +0100

    Updated comment

--- src/base58.h
+++ src/base58.h
@@ -250,7 +250,7 @@ public:
 };
 
 /** base58-encoded Bitcoin addresses.
- * Public-key-hash-addresses have version 0 (or 111 testnet).
+ * Public-key-hash-addresses have version 125 (or 127 testnet).
  * The data vector contains RIPEMD160(SHA256(pubkey)), where pubkey is the serialized public key.
  * Script-hash-addresses have version 5 (or 196 testnet).
  * The data vector contains RIPEMD160(SHA256(cscript)), where cscript is the serialized redemption script.
